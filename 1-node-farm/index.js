03d59209c737ebcaad764269a3f8235e3caa13e5
titi923 <petrisor.buciuta92@gmail.com>
2023-02-22 17:28:00 +0200

    reading and writing a file

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,1 +1,9 @@
 const fs = require('fs') // For the file system
+
+const textInput = fs.readFileSync('./txt/input.txt', 'utf-8')
+console.log(textInput);
+
+const textOutput = `This is what we know about avocado: ${textInput} Created on: ${Date.now()}` 
+
+fs.writeFileSync('./txt/output.txt', textOutput)
+console.log("File written");
