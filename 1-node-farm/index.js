737b0398ca4fd5fe3f919aa5041714ede0c0f46a
titi923 <petrisor.buciuta92@gmail.com>
2023-02-23 13:28:12 +0200

    Create a simple web server

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,4 +1,12 @@
 const fs = require('fs') // For the file system
 const http = require('http') // Gives us the networking capabilities
 
-const ser
+// First we create the server
+const server = http.createServer((req, res) => {
+  res.end('Hello from the server')
+})
+
+// Then we start the server
+server.listen(8000, '127.0.0.1', () => {
+  console.log("Listening to requests on port 8000 lol");
+})
