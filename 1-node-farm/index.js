51ccf6bf8c3aaaa2d2a7efbeca6f91773aab37af
titi923 <petrisor.buciuta92@gmail.com>
2023-02-23 13:23:09 +0200

    Create a simple Web Server

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,17 +1,4 @@
 const fs = require('fs') // For the file system
+const http = require('http') // Gives us the networking capabilities
 
-// BLOCKING, Synchronous way
-// const textInput = fs.readFileSync('./txt/input.txt', 'utf-8')
-// console.log(textInput);
-
-// const textOutput = `This is what we know about avocado: ${textInput} Created on: ${Date.now()}` 
-
-// fs.writeFileSync('./txt/output.txt', textOutput)
-// console.log("File written");
-
-
-// NON-BLOCKING, Asynchronous way
-fs.readFile('./txt/start.txt', 'utf-8', (err, data) => {
-  console.log(err);
-  console.log(data);
-})
+const ser
