8ca80542a9858697cf0e581b36dbf096029162b4
titi923 <petrisor.buciuta92@gmail.com>
2023-02-28 20:40:08 +0200

    006 Handling POST Requests (to add new tours to data)

--- 4-natours-project/app.js
+++ 4-natours-project/app.js
@@ -3,15 +3,8 @@ const express = require('express');
 
 const app = express();
 
-// app.get('/', (req, res) => {
-//   res
-//     .status(200)
-//     .json({ message: 'Hello from the server side!', app: 'Natours' });
-// });
-
-// app.post('/', (req, res) => {
-//   res.send('You can post to this endpoint...');
-// });
+// middleware (is a function that can modify the incoming request data)
+app.use(express.json());
 
 // Read the data first
 const tours = JSON.parse(
@@ -23,9 +16,32 @@ app.get('/api/v1/tours', (req, res) => {
     status: 'success',
     results: tours.length,
     data: {
-      tours
-    }
+      tours,
+    },
   });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  // Get the id of the last current element and increment it's id to make a new unique id
+  const newId = tours[tours.length - 1].id + 1;
+  // Assign a new object
+  const newTour = Object.assign({id: newId}, req.body)
+  // Then simply add the new object to the current array
+  tours.push(newTour);
+  // Finally modify the database/file, and we use an async function because we are in the event loop & NEVER EVER BLOCK THE EVENT LOOP!!!
+  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+    // What we want to do as soon as the file is written?
+    
+    // 200 OK, 201 Created
+    res.status(201).json({
+      status: "success",
+      data: {
+        tours: newTour
+      }
+    })
+  })
+  
+  // res.send('Done'); // No need to send the res twice
 });
 
 // Create a server that listen to a port
