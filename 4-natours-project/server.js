5448ccbd7eb52065de7ad5e9f9795424e67c855d
titi923 <petrisor.buciuta92@gmail.com>
2023-03-09 12:01:45 +0200

    Connecting our DataBase with Mongoose

--- 4-natours-project/server.js
+++ 4-natours-project/server.js
@@ -1,9 +1,20 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-dotenv.config({ path: './config.env' });
-
 const app = require('./app');
 
-// console.log(process.env);
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB)
+  .then((con) => {
+    console.log(con.connections);
+    console.log('DB Connection Succesful!');
+  });
 
 // Create a server that listen to a port
 const port = process.env.PORT || 8000;
